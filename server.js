58cdae7126de61bda9774d092a134b841625bf6a
olafk <olaf.koziara@gmail.com>
2020-11-24 15:54:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,14 +11,7 @@ const server = express()
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new WebSocket.Server({ server });
-const getUniqueID = () => {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-  return s4() + s4() + "-" + s4();
-};
+
 wss.on("connection", (ws) => {
   console.log("Client connected");
   ws.on("close", () => console.log("Client disconnected"));
