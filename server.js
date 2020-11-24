791017321e7c84252f7967a84d48bc1092a4d3d5
olafk <olaf.koziara@gmail.com>
2020-11-24 15:50:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const { Server } = require('ws');
+const express = require("express");
+const WebSocket = require("ws");
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const INDEX = "/index.html";
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const wss = new Server({ server });
-
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
-
-setInterval(() => {
+const wss = new WebSocket.Server({ server });
+const getUniqueID = () => {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + "-" + s4();
+};
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+  ws.on("close", () => console.log("Client disconnected"));
   wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
+    if (client !== ws) {
+      const uid = JSON.parse(data).uid;
+
+      if (uid === client.id) {
+        client.send(data);
+      }
+    }
   });
-}, 1000);
+});
